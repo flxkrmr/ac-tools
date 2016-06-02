b69f5165f23ed3b05eac4ccd28e06650e0d9a982
flxkrmr <felixkramerroki@aol.com>
2016-06-02 10:36:54 +0200

    fixed install linker

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-sudo ln -s wav2mp3.sh /sbin/wav2mp3
-sudo ln -s flac2mp3.sh /sbin/flac2mp3
+dir=$(pwd)
+
+sudo ln -s $dir/wav2mp3.sh /sbin/wav2mp3
+sudo ln -s $dir/flac2mp3.sh /sbin/flac2mp3
